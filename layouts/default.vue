114345e1ec131686e0d9eecfa5645e1624dafe0e
Marshall <mzmarshall1278@gmail.com>
2021-07-25 10:12:53 +0100

    add navbar and home page

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <navbar/>
+    <div class="pt-8 w-4/5 mx-auto">
+      <Nuxt/>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,51 @@
     <p class="text-center mb-4">Latest Crimes</p>
 
     <div class="md:grid grid-cols-2 xl:grid-cols-3 gap-5">
-      <div class="shadow shadow-xl mb-4 py-6 px-10 rounded" v-for="i in 5">
-        <p class="text-red-500 text-center">Case: Murder</p>
-        <p class="py-2">Date: 2/12/2021</p>
-        <p class="py-2">Time: 8:05pm</p>
-        <p class="py-2">Suspects: 4</p>
-        <p class="py-2">Witness: Media Footage(Video/Audio)</p>
-        <button class="bg-white text-green-500 py-1 px-4 rounded hover:bg-green-500 hover:text-white">view</button>
-      </div>
+      <crime-thumbnail-card v-for="crime in crimes" :key="crime.id" :crime="crime"/>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {}
+import crimeThumbnailCard from '~/components/crime-thumbnail-card.vue'
+export default {
+  components: { crimeThumbnailCard },
+  data(){
+    return {
+      crimes: [
+        {
+          id: 1,
+          type: 'Murder',
+          date: '12/02/2021',
+          time: '6:14pm',
+          evidence: 'Red Handed',
+          suspects: [
+            {name: 'zakzaky',},{name: 'shekau'}
+          ],
+        },
+        {
+          id: 2,
+          type: 'Rape',
+          date: '12/02/2021',
+          time: '9:10pm',
+          evidence: 'witness',
+          suspects: [
+            {name: 'shekau'}
+          ],
+        },
+        {
+          id: 3,
+          type: 'Robbery',
+          date: '19/02/2021',
+          time: '2:14am',
+          evidence: 'Media Footage(Video/Audio)',
+          suspects: [
+            {name: 'Abduljabbar'}
+          ],
+        }
+
+      ]
+    }
+  }
+  }
 </script>
